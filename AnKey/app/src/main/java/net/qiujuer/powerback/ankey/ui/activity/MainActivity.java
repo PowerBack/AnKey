324263d4b93dbf97db28bedf3cbad9997c651d9b
qiujuer <qiujuer@live.cn>
2015-10-25 16:01:18 +0800

    Change MainActivity layout

--- AnKey/app/src/main/java/net/qiujuer/powerback/ankey/ui/activity/MainActivity.java
+++ AnKey/app/src/main/java/net/qiujuer/powerback/ankey/ui/activity/MainActivity.java
@@ -1,25 +1,25 @@
 package net.qiujuer.powerback.ankey.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import net.qiujuer.genius.ui.widget.FloatActionButton;
 import net.qiujuer.powerback.ankey.R;
 import net.qiujuer.powerback.ankey.ui.SuperActivity;
+import net.qiujuer.powerback.ankey.widget.drawable.CrossLineShape;
 
-public class MainActivity extends SuperActivity {
+public class MainActivity extends SuperActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.text_hello).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateActivity.class));
-            }
-        });
+
+        initFloatActionButton();
     }
 
     @Override
@@ -44,4 +44,29 @@ public class MainActivity extends SuperActivity {
 
     }
 
+    private void initFloatActionButton() {
+        final float density = getResources().getDisplayMetrics().density;
+        FloatActionButton addButton = (FloatActionButton) findViewById(R.id.action_add);
+        CrossLineShape shape = new CrossLineShape();
+        ShapeDrawable drawable = new ShapeDrawable(shape);
+        Paint paint = drawable.getPaint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setColor(0xc0ffffff);
+        paint.setStrokeWidth(2 * density);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        drawable.setIntrinsicWidth(100);
+        drawable.setIntrinsicHeight(100);
+        addButton.setImageDrawable(drawable);
+        addButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.action_add) {
+            Intent intent = new Intent(MainActivity.this, CreateActivity.class);
+            startActivity(intent);
+        }
+    }
 }
